d2b1642b1f5047963ec35afea182d4dbd58934da
your username <dinak77240@gmail.com>
2019-06-04 15:23:44 +0200

    Add Spring

---
+++ src/main/java/fr/DoctorWho/DoctorWho/Controller/Doctors.java
@@ -0,0 +1,51 @@
+package fr.DoctorWho.DoctorWho.Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class Doctors {
+
+    public static void main(String[] args){
+        SpringApplication.run(Doctors.class, args);
+    }
+
+    @RequestMapping("/doctor/1")
+    @ResponseBody
+    public String firstDoctor(){
+        return "William Hartnell";
+    }
+
+    @RequestMapping("/doctor/6")
+    @ResponseBody
+    public String sixthDoctor(){
+        return "Colin Baker";
+    }
+
+    @RequestMapping("/doctor/9")
+    @ResponseBody
+    public String ninthDoctor(){
+        return "Christopher Eccleston";
+    }
+
+    @RequestMapping("/doctor/12")
+    @ResponseBody
+    public String twelfthDoctor(){
+        return "Peter Capaldi";
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "<ul> Doctor Who:" +
+                "<li><a href='/doctor/1'>First Doctor</a></li>" +
+                "<li><a href='/doctor/6'>Sixth Doctor</a></li>" +
+                "<li><a href='/doctor/9'>Ninth Doctor</a></li>" +
+                "<li><a href='/doctor/12'>Eleventh Doctor</a></li>" +
+                "</ul>";
+    }
+}
